f4e1de384ac106bd80e759dec2411b5b5f668b48
AhmedAshour683 <136949562+AhmedAshour683@users.noreply.github.com>
2023-07-03 10:05:05 +0300

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,80 @@
+
+function animateValue(obj1, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj1.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj1 = document.getElementById("value1");
+  animateValue(obj1, 0, 850, 1000);
+
+
+
+
+
+
+  function animateValue(obj2, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj2.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj2 = document.getElementById("value2");
+  animateValue(obj2, 0, 230, 1000);
+
+
+  
+
+
+
+
+  function animateValue(obj3, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj3.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj3 = document.getElementById("value3");
+  animateValue(obj3, 0, 9450, 1000);
+
+
+
+
+
+  function animateValue(obj4, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj4.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj4 = document.getElementById("value4");
+  animateValue(obj4, 0, 780, 1000);
+
